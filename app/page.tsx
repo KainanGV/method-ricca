b4d76c0932fb4c15211776a7b463ff1e42801e73
KainanGV <kainangabriel2019@gmail.com>
2024-09-06 21:14:27 -0300

    feat: remove example

--- app/page.tsx
+++ app/page.tsx
@@ -346,7 +346,7 @@ export default function Home() {
           <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
             <iframe
               className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
-              src="https://www.youtube.com/embed/BPZSQUIJWiI"
+              src="https://www.youtube.com/embed/tqyMQVbXP6A"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
